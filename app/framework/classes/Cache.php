c53f98bacdbdaeee82bdecbd04d25f0fa90a5e5f
Alexandre Cardoso <xandecar@hotmail.com>
2022-04-25 15:13:34 -0300

    Cache time removed from cache class

--- app/framework/classes/Cache.php
+++ app/framework/classes/Cache.php
@@ -4,11 +4,10 @@ namespace app\framework\classes;
 class Cache
 {
     private const CACHE_PREFIX = 'cache_';
-    private const CACHE_TIME = '+5 minutes';
 
     public static function cachePath(string $cacheName, string $content = ''):string
     {
-        $path = dirname(__FILE__,2);
+        $path = dirname(__FILE__, 2);
         $cacheName = self::CACHE_PREFIX.$cacheName;
         return "{$path}/resources/storage/cache/{$cacheName}.txt";
     }
@@ -17,12 +16,11 @@ class Cache
     {
         $cachePath = self::cachePath($cacheName);
 
-        if(!file_exists($cachePath) || strtotime("+ {$cacheValidation} minutes", filemtime($cachePath)) < strtotime('now')){
-            file_put_contents($cachePath,json_encode($callback()));
+        if (!file_exists($cachePath) || strtotime("+ {$cacheValidation} minutes", filemtime($cachePath)) < strtotime('now')) {
+            file_put_contents($cachePath, json_encode($callback()));
             return $callback();
         }
 
         return (array)json_decode(file_get_contents($cachePath));
     }
-
 }
