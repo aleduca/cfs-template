255b611aac6ea106fd1b4ada7910177f2b6eb84d
Alexandre Cardoso <xandecar@hotmail.com>
2022-04-25 11:26:50 -0300

    Better error catch

--- app/framework/classes/Engine.php
+++ app/framework/classes/Engine.php
@@ -3,10 +3,11 @@ namespace app\framework\classes;
 
 class Engine
 {
-    private string $layout;
+    private ?string $layout;
     private array $layoutData;
     private static $content;
-    private const TEMPLATE_EXTENSION = '.php';
+    private const TEMPLATE_EXTENSION = 'php';
+
     private function load()
     {
         if (self::$content) {
@@ -23,7 +24,7 @@ class Engine
     public function render(string $path, array $data = [])
     {
         try {
-            $path = getViewPath($path, 'php');
+            $path = getViewPath($path, self::TEMPLATE_EXTENSION);
 
             ob_start();
 
@@ -36,15 +37,14 @@ class Engine
             ob_end_clean();
 
             if (!empty($this->layout)) {
-                self::$content = $content;
-                $data = array_merge($this->layoutData, $data, );
-                $view = view($this->layout, $data);
-                return $view;
+                self::$content = $content; // content from template (e.g home or login)
+                $data = array_merge($this->layoutData, $data);
+                return view($this->layout, $data);
             }
 
             return $content;
         } catch (\Throwable $th) {
-            echo $th->getMessage();
+            echo $th->getMessage(). ' '.$th->getFile() . ' '.$th->getLine();
         }
     }
 }
