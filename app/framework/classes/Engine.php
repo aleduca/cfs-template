590659a1b3a946f39bcef4fd316ca0be0a954e34
Alexandre Cardoso <xandecar@hotmail.com>
2022-04-25 10:21:22 -0300

    Check if template exist

--- app/framework/classes/Engine.php
+++ app/framework/classes/Engine.php
@@ -6,10 +6,15 @@ class Engine
     private string $layout;
     private array $layoutData;
     private static $content;
+    private const EXTENSION = '.php';
 
     private function getViewPath(string $file):string
     {
-        return dirname(__FILE__, 2).DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.$file.'.php';
+        $file = dirname(__FILE__, 2).DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.$file.self::EXTENSION;
+        if (!file_exists($file)) {
+            throw new \Exception("Template {$file} does not exist");
+        }
+        return $file;
     }
 
     private function load()
@@ -21,33 +26,35 @@ class Engine
 
     private function extends(string $layout, array $data = [])
     {
-        $this->layout = $this->getViewPath($layout);
+        $this->layout = $layout;
         $this->layoutData = $data;
     }
 
     public function render(string $path, array $data = [])
     {
-        if (!file_exists($path)) {
+        try {
             $path = $this->getViewPath($path);
+
+            ob_start();
+
+            extract($data);
+        
+            require $path;
+        
+            $content = ob_get_contents();
+        
+            ob_end_clean();
+
+            if (!empty($this->layout)) {
+                self::$content = $content;
+                $data = array_merge($this->layoutData, $data, );
+                $view = view($this->layout, $data);
+                return $view;
+            }
+
+            return $content;
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
         }
-
-        ob_start();
-
-        extract($data);
-        
-        require $path;
-        
-        $content = ob_get_contents();
-        
-        ob_end_clean();
-
-        if (!empty($this->layout)) {
-            self::$content = $content;
-            $data = array_merge($this->layoutData, $data, );
-            $view = view($this->layout, $data);
-            return $view;
-        }
-
-        return $content;
     }
 }
