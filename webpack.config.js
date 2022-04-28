1d0a6b1bfb989dfa5599133d884e44bcf4606ece
Alexandre Cardoso <xandecar@hotmail.com>
2022-04-27 21:06:23 -0300

    Added support to webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+     mode: process.env.NODE_ENV ,
+     devtool: process.env.NODE_ENV == 'development' ? 'source-map' : false,
+     entry: {
+          app: ['./public/assets/app.js'],
+     },
+     output: {
+          path: path.resolve(__dirname, 'public'),
+          filename: '[name].js',
+     },
+     module: {
+          rules: [
+               {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+               },
+          ],
+     },
+};
